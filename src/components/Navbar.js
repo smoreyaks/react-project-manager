daee73ed3e96e48ca1b9864612915b2f7430979e
smoreyaks <bnixon.eng@gmail.com>
2022-09-09 20:19:42 +1000

    Create Navbar Template

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+// Styles
+import styles from './Navbar.css'
+
+export default function Navbar() {
+  return (
+    <div>Navbar</div>
+  )
+}
