27c735819a36de2b9a2b47a42f5f8c35c4f92ea6
smoreyaks <bnixon.eng@gmail.com>
2022-09-12 23:39:03 +1000

    Create Sign Up Form

--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -1,10 +1,60 @@
+// Hooks
+import { useState } from 'react'
+
 // Styles
 import './Signup.css'
 
 
 export default function Signup() {
-    
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [displayName, setDsiplayName] = useState('')
+    const [thumbnail, setThumbnail] = useState(null)
+
     return (
-        <div>Signup</div>
+        <div className='auth-form'>
+            <form>
+                <h2>Sign Up</h2>
+                <label>
+                    <span>Email:</span>
+                    <input 
+                        type="email"
+                        onChange={(e) => setEmail(e.target.value)}
+                        value={email}
+                        required
+                        />
+                </label>
+
+                <label>
+                    <span>Password:</span>
+                    <input 
+                        type="text"
+                        onChange={(e) => setPassword(e.target.value)}
+                        value={password}
+                        required
+                        />
+                </label>
+                <label>
+                    <span>Display Name:</span>
+                    <input 
+                        type="text"
+                        onChange={(e) => setDsiplayName(e.target.value)}
+                        value={displayName}
+                        required
+                    />
+                </label>
+                <label>
+                    <span>Profile Thumbnail:</span>
+                    <input 
+                        type="file"
+                        // onChange={(e) => setThumbnail(e.target.value)}
+                        // value={thumbnail}
+                        required
+                    />
+                </label>
+                <button className='btn'>Sign Up</button>
+                
+            </form>
+        </div>
     )
 }
