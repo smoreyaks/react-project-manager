de5e18e0bcd6936dd43c772ef5724a8a65f623f8
smoreyaks <bnixon.eng@gmail.com>
2022-09-16 19:49:10 +1000

    Handling File Inputs on Sign Up

--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -10,6 +10,36 @@ export default function Signup() {
     const [password, setPassword] = useState('')
     const [displayName, setDsiplayName] = useState('')
     const [thumbnail, setThumbnail] = useState(null)
+    const [thumbnailError, setThumbnailError] = useState(null)
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(email, password, displayName, thumbnail)
+    }
+
+    const handleFileChange = (e) => {
+        setThumbnail(null)
+        let selected = e.target.files[0]
+        console.log(selected)
+        
+        if (!selected) {
+            setThumbnailError('Please select a file')
+            return
+        }
+        if (!selected.type.includes('image')) {
+            setThumbnailError('Selected file must be an image')
+            return
+        }
+        if (selected.size > 100000) {
+            setThumbnailError('Image file size must be less than 100kb')
+            return
+        }
+
+        setThumbnailError(null)
+        setThumbnail(selected)
+        console.log('Thumbnail Updated')
+    }
+
 
     return (
         <div className='auth-form'>
@@ -22,7 +52,7 @@ export default function Signup() {
                         onChange={(e) => setEmail(e.target.value)}
                         value={email}
                         required
-                        />
+                    />
                 </label>
 
                 <label>
@@ -48,9 +78,11 @@ export default function Signup() {
                     <input 
                         type="file"
                         // onChange={(e) => setThumbnail(e.target.value)}
+                        onChange={handleFileChange}
                         // value={thumbnail}
                         required
                     />
+                    { thumbnailError && <div className="error">{ thumbnailError }</div> }
                 </label>
                 <button className='btn'>Sign Up</button>
                 
