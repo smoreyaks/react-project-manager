9f8a816ae3618b7b02120a3981e0b20b475f4873
smoreyaks <bnixon.eng@gmail.com>
2022-10-31 15:09:09 +1100

    Making A Project List (Grid)

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,20 +1,40 @@
-// Style
-import './ProjectList.css'
-
 // React
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 // Hooks
 import { useCollection } from '../hooks/useCollection'
 
+// Style
+import './ProjectList.css'
+
+// Components
+import Avatar from './Avatar'
+
 export default function ProjectList({ projects }) {
     
     return (
-        <div>
+        <div className='project-list'>
             { projects.length === 0 && <p>No projects yet!</p> }
             { projects.map(project => (
-                <div key={ project.id }>{ project.name }</div>
+                <Link 
+                    to={`/projects/${project.id}`} 
+                    key={ project.id }
+                >
+                    <h4>{project.name}</h4>
+                    <p>Due by {project.dueDate.toDate().toDateString()}</p>
+                    <div className="assigned-to">
+                        <ul>
+                            {project.assignedUsersList.map(user => (
+                                <li key={user.photoURL}>
+                                    <Avatar src={user.photoURL}/>
+                                </li>
+                                ))}
+                        </ul>
+                    </div>
+                </Link>
             ))}
+            <br />
         </div>
     )
 }
