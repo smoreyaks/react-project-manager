4b285296b95ad90c2abc3422ea9e14181297b81f
smoreyaks <bnixon.eng@gmail.com>
2022-11-05 13:03:13 +1100

    Project Filter - Project Assigned Working

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,5 +1,5 @@
 // Hooks
-import {useCollection } from '../../hooks/useCollection'
+import { useCollection } from '../../hooks/useCollection'
 import { useState } from 'react'
 import { useAuthContext } from '../../hooks/useAuthContext'
 
@@ -9,6 +9,7 @@ import './Dashboard.css'
 // Components
 import ProjectList from '../../components/ProjectList'
 import ProjectFilter from './ProjectFilter'
+
 import userEvent from '@testing-library/user-event'
 
 export default function Dashboard() {
@@ -22,38 +23,33 @@ export default function Dashboard() {
 
     const projects = documents ? documents.filter(document => {
         switch (filter) {
-            case "All": 
+            case 'All': 
                 return true;
-            
-            case "Owned": 
-                let assignedToMe = false;
-                document.assignedUsersList.forEach((u) => {
-                    if (user.uid === u.id) {
+            case 'Assigned': 
+                let assignedToMe = false
+                document.assignedUsersList.forEach(u => {
+                    if(u.id === user.uid) {
                         assignedToMe = true
                     }
                 })
-                return assignedToMe;
-
-            case "Development": 
-            case "Design": 
-            case "Sales": 
-            case "Marketing": 
+                return assignedToMe
+            case 'Created By':
+            case 'Development':
+            case 'Design':
+            case 'Sales':
+            case 'Marketing':
                 console.log(document.category, filter)
                 return document.category === filter
-            
-            default :
+            default:
                 return true
-
-        }
-    }) : null
+            }
+        }) : null
 
     return (
         <div>
             <h2 className="page-title">Dashboard</h2>
             { error && <p className="error">{error}</p> }
-            { documents && (
-                <ProjectFilter currentFilter={filter} changeFilter={changeFilter} />
-            )}
+            { documents && <ProjectFilter changeFilter={changeFilter} /> }
             { projects && <ProjectList projects={projects} /> }
         </div>
     )
--- src/pages/Dashboard/ProjectFilter.js
+++ src/pages/Dashboard/ProjectFilter.js
@@ -4,18 +4,25 @@ import React from 'react'
 // Hooks
 import { useState } from 'react'
 
+const filterList = [
+                        'All', 
+                        'Assigned', 
+                        'Created By', 
+                        'Development', 
+                        'Design', 
+                        'Marketing', 
+                        'Sales'
+                    ]
 
-const filterList = ['All', 'Owned', 'Development', 'Design', 'Marketing', 'Sales']
-
-export default function ProjectFilter({currentFilter, changeFilter}) {
-
+export default function ProjectFilter({ changeFilter }) {
+    const [currentFilter, setCurrentFilter] = useState('all')
     
-
     const handleClick = (newFilter) => {
+        setCurrentFilter(newFilter)
         changeFilter(newFilter)
     }
+
     return (
-        
         <div className='project-filter'>
             <nav>
                 <p>Filter By:</p>
