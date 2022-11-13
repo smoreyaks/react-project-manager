65f4165d3c6ea1fb4594ca68e8d896c5cb744463
smoreyaks <bnixon.eng@gmail.com>
2022-11-13 11:27:11 +1100

    Renamed Config.js to config.js

---
+++ src/firebase/config.js
@@ -0,0 +1,28 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+import 'firebase/storage'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCJYQcdrQb7S3snukrZAMwM_vAqPqGc4MU",
+    authDomain: "we-build-58488.firebaseapp.com",
+    projectId: "we-build-58488",
+    storageBucket: "we-build-58488.appspot.com",
+    messagingSenderId: "1020057690906",
+    appId: "1:1020057690906:web:a2ccf4684a742111a402a3"
+};
+
+
+// Initialise Firebase
+firebase.initializeApp(firebaseConfig)
+
+// Initialise Services
+const projectFirestore = firebase.firestore()
+const projectAuth = firebase.auth()
+
+const projectStorage = firebase.storage()
+
+// timestamp
+const timestamp = firebase.firestore.Timestamp
+
+export { projectFirestore, projectAuth, projectStorage, timestamp  }
